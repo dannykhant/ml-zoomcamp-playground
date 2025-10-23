725cd86668c2c7449b9dda12f29483adc2891feb
dannykhant <dannypmkhant@gmail.com>
2025-10-23 22:13:28 +0700

    Add churn web service

--- 05-deployment/legacy_project/predict.py
+++ 05-deployment/legacy_project/predict.py
@@ -9,9 +9,9 @@ input_file = "model_C=1.0.bin"
 with open(input_file, "rb") as f_in:
     dv, model = pickle.load(f_in)
 
-app = Flask()
+app = Flask("predict")
 
-@app.route("/predict", methods=["GET"])
+@app.route("/predict", methods=["POST"])
 def predict():
     customer = request.get_json()
 
@@ -25,3 +25,6 @@ def predict():
     }
 
     return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9696)
