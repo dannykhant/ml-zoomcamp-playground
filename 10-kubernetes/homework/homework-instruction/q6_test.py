2543e18ad2a89dd290949f9b7e29f8fb8acb97c2
dannykhant <dannypmkhant@gmail.com>
2025-12-11 20:51:19 +0700

    feat: [k8s] add homework instruction

---
+++ 10-kubernetes/homework/homework-instruction/q6_test.py
@@ -0,0 +1,12 @@
+from time import sleep
+import requests
+
+
+url = "http://localhost:9696/predict"
+
+client = {"job": "management", "duration": 400, "poutcome": "success"}
+
+while True:
+    sleep(0.1)
+    response = requests.post(url, json=client).json()
+    print(response)
